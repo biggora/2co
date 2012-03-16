b6328c0dd93f0d75723f810fe7d3f1c5a22c40e9
Alexey Gordeyev <aleksej@gordejev.lv>
2012-03-16 09:01:51 +0200

    Update tests

---
+++ tests/conf.js
@@ -0,0 +1,15 @@
+/**
+ * @created     2012-03-11 17:19:10
+ * @category    Payments Gatways
+ * @package     2CO
+ * @version     0.0.1
+ * @copyright   Copyright (c) 2009-2011 - All rights reserved.
+ * @license     MIT License
+ * @author      Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @link        http://www.gordejev.lv
+ */
+
+module.exports = {
+        user: 'your login',
+        pass: 'your pass'
+};

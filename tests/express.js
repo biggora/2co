158f0aff9a90364a4fbce0107e29a7bef0c85c1c
Alexey Gordeyev <aleksej@gordejev.lv>
2012-03-17 21:54:17 +0200

    Update express tests

--- tests/express.js
+++ tests/express.js
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 var checkout = require("./../../2co"),
 express = require('express'),
@@ -14,23 +9,50 @@ app.use(express.methodOverride());
 app.use(app.router);
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.send('Hello 2CO');
 });
 
-checkout.notificationCallback(function(req,res){
+/*
+ * Receive messages from 2CO Instant Notification System
+ * Variant 1
+ */
+checkout
+    .notificationRoute('/notifications') // default route '/payments/2co/notifications'
+    .notificationCallback(function(req,res){
+        var data = req.query || {};
+        res.send(data);
+    }).notificationHelper(app);
+/*
+
+ Variant 2
+
+function callback(req,res){
     var data = req.query || {};
-    res.send('Custom Callback');
-});
+    res.send(data);
+}
+checkout
+    .notificationRoute('/notifications')
+    .notificationHelper(app, callback);
 
-checkout.notificationHelper(app);
+*/
+
+/* Receive messages from 2CO Instant Notification System */
+checkout
+    .scriptRoute('/2co/:pid?')  // default route '/payments/2co/callback/:pid?'
+    .scriptCallback(function(req,res){
+        var data = req.query || {},
+        pid = req.params.pid;
+        res.send(data);
+    }).scriptHelper(app);
+/*
+
+function callback(req,res){
+    var data = req.query || {};
+    res.send(data);
+}
+checkout.scriptHelper(app, callback);
+
+*/
+
 
 app.listen(port,host);
-/*
-var app = require('express').createServer();
-
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-
-app.listen(3000);
- */

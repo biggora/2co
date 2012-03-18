d61c980d46e34a111e2aa070c1fe9d8b3141e630
Alexey Gordeyev <aleksej@gordejev.lv>
2012-03-18 13:07:36 +0200

    Update Shopping Cart

--- tests/express.js
+++ tests/express.js
@@ -14,19 +14,42 @@ app.get('/', function(req, res) {
 
 /*
  * Receive messages from 2CO Instant Notification System
- */
+
 checkout
     .notificationRoute('/notifications') // default route '/payments/2co/notifications'
     .fetchNotification(function(data,res){
         res.send(data);
     })
     .notificationHelper(app);
-
-/* Receive callbacks if order pending */
+*/
+/* Receive callbacks if order pending
 checkout
     .scriptRoute('/2co/:pid?')  // default route '/payments/2co/callback/:pid?'
     .scriptCallback(function(pid,data,res){
         res.send(data);
     }).scriptHelper(app);
+
+checkout
+    .notificationRoute('/notifications') // default route '/payments/2co/notifications'
+    .fetchNotification(function(data,res){
+        res.send(data);
+    })
+    .notificationHelper(app);
+*/
+
+checkout
+    .scriptRoute('/2co/:pid?')  // default route '/payments/2co/callback/:pid?'
+    .scriptCallback(function(pid,data,res){
+        res.send(data);
+    })
+    .notificationRoute('/notifications')
+    .fetchNotification(function(data,res){
+        res.send(data);
+    })
+    .shoppingCartRoute('/cart')
+    .shoppingCartFiller(function(sess,data,cart,res){
+        res.send(data);
+    })
+    .expressHelper(app);
 
 app.listen(port,host);

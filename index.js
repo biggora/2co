48a9aa0db86e30de92429bdd046883ec6b622c42
Alexey Gordeyev <aleksej@gordejev.lv>
2012-03-24 19:04:47 +0200

    Fix version

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+/**
+ * @created     2012-03-11 16:10:00
+ * @category    Payments Gatways
+ * @package     2CO
+ * @version     0.0.3e
+ * @copyright   Copyright (c) 2009-2012 - All rights reserved.
+ * @license     MIT License
+ * @author      Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @link        http://www.gordejev.lv
+ */
 
 module.exports = require('./lib/2co');
